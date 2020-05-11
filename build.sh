6b2cbcfe8bb768fbbfd216908c0d88df4b359244
Exhibition <online-exhibition@dein-hosting.de>
2020-05-11 19:13:31 +0200

    Bump version 0.2

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-cd production
-docker build -f Dockerfile -t exhibit-gateway:0.1 .
-docker tag exhibit-gateway:0.1 docker.pkg.github.com/online-exhibition/exhibition/exhibit-gateway:0.1
-docker push docker.pkg.github.com/online-exhibition/exhibition/exhibit-gateway:0.1
+cd $SCRIPT_PATH/../ui
+npm run build
+
+cd $SCRIPT_PATH/production
+docker build -f Dockerfile -t exhibit-gateway:0.2 .
+docker tag exhibit-gateway:0.2 docker.pkg.github.com/online-exhibition/exhibition/exhibit-gateway:0.2
+docker push docker.pkg.github.com/online-exhibition/exhibition/exhibit-gateway:0.2
